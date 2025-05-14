4a95dc7f7acbda8321937d96c9b4f1dad0ddefd3
Mahla Khorashadi <mahla.kh09923531595@gmail.com>
2025-05-14 23:39:01 +0330

    add not fixed balataneAnterior

--- src/Firstpage.jsx
+++ src/Firstpage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Playground from "./Playground";
 import BalatanePosht from "./patterns/BalatanePosht";
 import PropertiesPanel from "./PropertiesPanel";
+import BalataneAnterior from "./patterns/BalataneAnterior";
 
 export default function Firstpage() {
   return (
     <div className="full">
       <Playground>
         <BalatanePosht />
+        <BalataneAnterior />
       </Playground>
       <PropertiesPanel />
     </div>
